ee6a899b519acd52bba3fc4ad6c8aca17f0ec3bc
khalilnazari <khalil.deve@gmail.com>
2023-07-02 12:22:06 +0800

    Roles module is added

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,5 +1,5 @@
 export const jwtConstants = {
   secret:
     '4x$1ma5txh$8uc0n3u*bx*optrlqdi@q=!5q8!+98irkprx2o0fv2j!=guk#6-!e)k#20k^%j4nx*k5q+%*9c0k5yfv+jojzmbx^*8hyjgfk@$@mi3b%^*wj8m8sla=@^kx^(u7m+^qb@v))@#4ws)*8hyjgfk@$@mi3b%^*wj8m8sla=@^kx^(u7m+^qb@v))@#4ws)',
-  secretKeyExpiry: '60s',
+  secretKeyExpiry: '1h',
 };
