c77ba7e8690d01c2b1e872b3093442d01d9d00cd
khalilnazari <khalil.deve@gmail.com>
2024-05-05 22:13:37 +0800

    Added simple middleware

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { LogMiddleware } from 'src/middlewares/log.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
@@ -10,4 +16,12 @@ import { User } from './entities/user.entity';
   providers: [UserService],
   exports: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  // Apply middleware
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LogMiddleware)
+      .exclude({ path: 'user', method: RequestMethod.GET })
+      .forRoutes(UserController);
+  }
+}
